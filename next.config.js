00ab6aed7dd789354ed37dcbc505b1af5d36cd4d
ayush2390 <tsayush2390@gmail.com>
2023-07-26 18:18:56 +0530

    Created UI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  // Your existing Next.js configuration goes here...
+  nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/fonts/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://fonts.googleapis.com", // Allow requests from the Google Fonts domain
+          },
+        ],
+      },
+    ];
+  },
+};
