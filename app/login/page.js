0d7c2c7cc2ca0f14462b64a009991530606340de
yung-coder <chandel.sanjeev5@gmail.com>
2023-08-05 14:43:31 +0530

    login

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import Login from "./Login";
+
+
+export default function Page () {
+  return ( 
+    <div className="flex h-screen  bg-gray-300  justify-center items-center">
+      <Login />
+    </div>
+  )
+} 
