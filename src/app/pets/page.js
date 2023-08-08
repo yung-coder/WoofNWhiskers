085e6770cb029c11f098a19844a13bb0abe6c66c
yung-coder <chandel.sanjeev5@gmail.com>
2023-08-09 02:47:46 +0530

    db fetch

--- src/app/pets/page.js
+++ src/app/pets/page.js
@@ -1,7 +1,22 @@
-export default function Page() {
-   return (
-     <div>
-       <h1>PETS</h1>  
-     </div>
-   )
+import { supabaseClient } from "../../../supabase/supabase";
+
+async function getData() {
+
+  let { data, error } = await supabaseClient.from("animal ").select("*");
+
+  console.log(error);
+
+  return data;
+}
+
+
+export default async function Page() {
+  const result = await getData();
+ console.log(result);
+
+  return (
+    <div>
+      <h1>PETS</h1>
+    </div>
+  );
 }
--- supabase/supabase.js
+++ supabase/supabase.js
@@ -4,7 +4,7 @@ const supbase = createClient(
   "https://mkoqckwtgsykoavjkmox.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rb3Fja3d0Z3N5a29hdmprbW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAyOTgxNDgsImV4cCI6MjAwNTg3NDE0OH0.qx-nejLCK-U56FbZgFxGxNKYKzKqyOz8yz77CMv0LDA",
   {
-    auth: { persistSession: true },
+    auth: { persistSession: false },
   },
 );
 
