bed74eb125209908ff88a96902b52e313815476c
ayush2390 <tsayush2390@gmail.com>
2023-08-12 22:39:36 +0530

    individual pet page

--- src/app/pets/Pets.js
+++ src/app/pets/Pets.js
@@ -9,31 +9,115 @@ import { supabaseClient } from "../../../supabase/supabase";
 
 const Pets = () => {
   const [visibility, setVisibility] = useState(0);
+  const [states, setStates] = useState([]);
+  const [animalType, setAnimalType] = useState(null);
+  const [Country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+  const [countryList, setCountryList] = useState([]);
+  // const [zipcode, setZipcode] = useState(null);
+  // const [zipcodeList, setZipcodeList] = useState([]);
   const [pets, setpets] = useState([]);
+  const [originalPets, setOriginalPets] = useState([]);
   const getData = async () => {
-    let { data  } = await supabaseClient
-      .from("animals")
-      .select("*");
-
+    let { data } = await supabaseClient.from("animals").select("*");
+    setOriginalPets(data);
     setpets(data);
   };
 
   useEffect(() => {
     getData();
   }, []);
+  console.log(pets);
 
   const handleVisibility = () => {
     const visibleValue = visibility == 0 ? 1 : 0;
     setVisibility(visibleValue);
   };
-  console.log(visibility);
+  const handleAnimalChange = (e) => {
+    const selectedAnimalType = e.target.value;
+    setAnimalType(selectedAnimalType);
+  };
+  const handleCountryChange = (e) => {
+    const selectedCountry = e.target.value;
+    setCountry(selectedCountry);
+  };
+  const handleStateChange = (e) => {
+    const selectedState = e.target.value;
+    setState(selectedState);
+  };
+  // const handleZipcodeChange = (e) => {
+  //   const selectedZipCode = e.target.value;
+  //   setZipcode(selectedZipCode);
+  // };
+
+  useEffect(() => {
+    originalPets?.map((pet) => states.push(pet.state));
+    let set = new Set(states);
+    setStates([...set]);
+  }, []);
+  console.log(states);
+  useEffect(() => {
+    originalPets?.map((pet) => countryList.push(pet.country));
+    let set = new Set(countryList);
+    setCountryList([...set]);
+  }, []);
+  console.log(countryList);
+  // useEffect(() => {
+  //   originalPets?.map((pet) => zipcodeList.push(pet.zipcode));
+  //   let set = new Set(zipcodeList);
+  //   setZipcodeList([...set]);
+  // }, []);
+
+  // console.log(zipcodeList);
+  useEffect(() => {
+    let modifiedPets = originalPets.filter((pet) => pet.country == Country);
+    setpets(modifiedPets);
+  }, [Country]);
+  useEffect(() => {
+    let modifiedPets = originalPets.filter((pet) => pet.type == animalType);
+    setpets(modifiedPets);
+  }, [animalType]);
+  useEffect(() => {
+    let modifiedPets = originalPets.filter((pet) => pet.state == state);
+    setpets(modifiedPets);
+  }, [state]);
+  // useEffect(() => {
+  //   let modifiedPets = originalPets.filter((pet) => pet.zipcode == zipcode);
+  //   setpets(modifiedPets);
+  // }, [zipcode]);
+
   return (
     <div className="mainpets">
       <Header title={"Available Pets for Adoption"} />
+      <div>
+        <select id="filter" onChange={handleAnimalChange}>
+          <option value="all">Select Animal Type</option>
+          <option value="dog">Dog</option>
+          <option value="cat">Cat</option>
+        </select>{" "}
+        <select id="filter" onChange={handleCountryChange}>
+          <option value="all">Select Country</option>
+          {countryList.map((country) => {
+            return <option value={country}>{country}</option>;
+          })}
+        </select>{" "}
+        <select id="filter" onChange={handleStateChange}>
+          <option value="all">Select State</option>
+          {states.map((state) => {
+            return <option value={state}>{state}</option>;
+          })}
+        </select>{" "}
+        {/* <select id="filter">
+          <option value="all">Select Zipcode</option>
+          {zipcodeList.map((zipcode) => {
+            return <option value={zipcode}>{zipcode}</option>;
+          })}
+        </select>{" "} */}
+      </div>
       <div className="petsContainer">
         <div className="flex flex-wrap w-screen  p-5 justify-start ">
           {pets.map((pet, index) => (
-            <Link href={`/pets/${index.toString()}`}>
+            <Link href={`/pets/${pet.id}`}>
               <PetsProfileDisplay
                 petType={pet.type}
                 location={pet.city}
--- src/app/pets/[slug]/page.js
+++ src/app/pets/[slug]/page.js
@@ -1,41 +1,140 @@
 "use client";
-
+import { supabaseClient } from "../../../../supabase/supabase";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LanguageIcon from "@mui/icons-material/Language";
+import MaleIcon from "@mui/icons-material/Male";
+import CallIcon from "@mui/icons-material/Call";
+import UpdateIcon from "@mui/icons-material/Update";
+import FemaleIcon from "@mui/icons-material/Female";
 import { usePathname } from "next/navigation";
 const page = () => {
   const pathname = usePathname();
+  const [pets, setpets] = useState([]);
+  const [modifiedPets, setModifiedPets] = useState([]);
+  const [current, setCurrent] = useState(0);
+
+  const getData = async () => {
+    let { data } = await supabaseClient.from("animals").select("*");
+    setpets(data);
+  };
+
   //   const { slug } = router.query;
-  console.log(pathname.replace("/pets/", ""));
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(pets);
+  let id = pathname.replace("/pets/", "");
+  console.log(id);
+  useEffect(() => {
+    console.log("modifiedPets");
+    if (pets.length !== 0) {
+      let petsList = pets.filter((pet) => pet.id == id);
+      setModifiedPets(petsList);
+      console.log(modifiedPets);
+    }
+  }, [pets]);
+  console.log(modifiedPets);
+  const SliderData = [
+    {
+      image: modifiedPets[0]?.image,
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    },
+  ];
+  const length = SliderData.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
+    return null;
+  }
+  function capitalizeFirstLetter(str) {
+    return str?.charAt(0).toUpperCase() + str?.slice(1);
+  }
+  let animalType = modifiedPets[0]?.type;
+
   return (
     <div className="petsDetailContainer">
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+        {SliderData.map((slide, index) => {
+          return (
+            <div
+              className={index === current ? "slide active" : "slide"}
+              key={index}
+            >
+              {index === current && (
+                <img src={slide.image} alt="travel image" className="image" />
+              )}
+            </div>
+          );
+        })}
+      </section>
       <div className="PetsModalContainer">
-        <div className="PetsModalInfo">
-          <p>
-            Pet Type: <span>Dog</span>
+        <div className="PetsInfo">
+          <p className="PetsInfoType">
+            <span>{capitalizeFirstLetter(animalType)}</span>
           </p>
-          <p>
-            Country: <span>India</span>
+          <p className="PetsInfoHealth">
+            <span>
+              {modifiedPets[0]?.gender == "Male" ? (
+                <MaleIcon />
+              ) : (
+                <FemaleIcon />
+              )}
+              {modifiedPets[0]?.gender}
+            </span>{" "}
+            <span>
+              <UpdateIcon />
+              {modifiedPets[0]?.age} years
+            </span>{" "}
+            <span>x months</span>
           </p>
-          <p>
-            City: <span>Delhi</span>
+          <hr />
+
+          <p className="PetsInfoAddress">
+            <LocationOnIcon />
+            {modifiedPets[0]?.address}
           </p>
-          <p>
-            Address: <span>abc building, xyz state, 110044</span>
+          <p className="PetsInfoCountry">
+            <LanguageIcon />
+            {modifiedPets[0]?.state}({modifiedPets[0]?.country})
           </p>
-          <p>
-            Contact number: <span>xxxxxxx</span>
+          <p className="PetsInfoContact">
+            <CallIcon />
+            xxxxxxx
           </p>
-          <p>
-            Pet breed: <span>xxxxxxx</span>
+          <hr />
+          <p className="PetsInfoBio">About</p>
+          <p className="PetsInfoBioData">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
           </p>
-          <p>
-            Pet age: <span>xxxxxxx</span>
-          </p>
-        </div>
-        <div className="PetsModalImage">
-          <img
-            src="https://media.istockphoto.com/id/541833910/vector/dog-and-cat-icon.jpg?s=2048x2048&w=is&k=20&c=DioFBfQqgEDoZ7cUP9zUqVpFDWOVUAZ6xQFzNl-jjh8="
-            alt=""
-          />
         </div>
       </div>
     </div>
